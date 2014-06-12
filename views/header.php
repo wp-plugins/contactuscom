25b1679064921f097567bfe1c793c4237e829b7e
contactus.com <contactus.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-12 19:19:35 +0000

    Contact Form by  ContactUs
    New version to branches
    
    = 5.0.0 =
    * Major Version Release
    * NEW: Place multiple forms across your site
    * NEW: Complete UI redesign
    * NEW: More Customization Options
    * NEW: Better Deeplink Integration
    * NEW: Easy Toggle yes and no options
    * NEW: Improved Widget Functions, quick Form Key implementation
    * NEW: Improved Setup Process
    * Improved on-site settings
    * *Launch date 12.06.2014*
    
    git-svn-id: https://plugins.svn.wordpress.org/contactuscom/trunk@931392 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * CONTACT FORM BY CONTACTUS.COM
+ *
+ * Initialization Plugin Header
+ * @since 5.0 First time this was introduced into plugin.
+ * @author ContactUs.com <support@contactus.com>
+ * @copyright 2014 ContactUs.com Inc.
+ * Company      : contactus.com
+ * Updated  	: 20140602
+ * */
+?>
+
+<div class="cUsCF_preloadbox"><div class="cUsCF_loadmessage"><span class="loadingBx"></span></div></div>
+<span class="save_message_placement"><p></p></span>
+<div id="cu_header">
+    <div class="row">
+        <div class="col-sm-12">
+            <a href="admin.php?page=cUs_form_plugin" title="<?php echo cUsCF_PLUGINNAME; ?>"><img src="<?php echo plugins_url('assets/img/contactus.logo.png', dirname(__FILE__) ) ;  ?>" alt="<?php echo cUsCF_PLUGINNAME; ?>"></a>
+            <span>|</span>
+            <span><?php echo cUsCF_PLUGINNAME; ?></span>
+        </div>
+    </div>
+</div>
