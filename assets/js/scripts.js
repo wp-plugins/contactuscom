ddbb401f002fe4cd0a9d64f6a622f62b112ad46c
contactus.com <contactus.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-22 19:32:23 +0000

    Contact Form Plugin
    = 5.0.2 =
    * BUG-FIX: Fixed short code rendering in plugin (Special fix for short code users)
    * *Launch date 22.08.2014*
    
    git-svn-id: https://plugins.svn.wordpress.org/contactuscom/trunk@970958 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/scripts.js
@@ -0,0 +1,79 @@
+
+//PLUGIN $ ENVIROMENT ($)
+var cUsCF_myjq = jQuery.noConflict();
+
+cUsCF_myjq(window).error(function(e){
+    e.preventDefault();
+});
+
+//ON READY DOM LOADED
+cUsCF_myjq(document).ready(function($) {
+
+    try{
+        //LOADING UI BOX
+        $( ".cUsCF_preloadbox" ).delay(1500).fadeOut('slow', function(){
+
+        // START THE INTRO.JS INTROJS
+        //startIntro();
+        // END THE INTRO.JS INTROJS
+
+        });
+        $('.tooltips').tooltip();
+        
+        //UI TABS
+        //$( "#cUsCF_tabs" ).tabs({active: false});
+        //$( "#menuWrapper" ).tabs({active: false});
+        //$('#tabs').tabs();
+        //console.log('tabs loaded')
+
+        //UNBIND UI TABS LINK ON CLICK
+        $("li.gotohelp a").unbind('click');
+
+        //colorbox window
+        $(".tooltip_formsett").colorbox({iframe:true, innerWidth:'75%', innerHeight:'80%'});
+
+        cUsCF_myjq('.sign-in').click(function() {
+            cUsCF_myjq('.signup-form').slideToggle('slow');
+            cUsCF_myjq('.login-form').slideToggle('slow');
+        });
+
+        $(".custom-checkbox").bootstrapSwitch({
+            onColor: 'success',
+            offColor: 'danger',
+            size: 'small'
+        });
+
+       
+    }catch(err){
+        console.log(err);
+        $('.advice_notice').html('Error - please update your WordPress  to the latest version. If the problem continues, contact us at support@contactus.com.: ' + err ).slideToggle().delay(2000).fadeOut(2000);
+    }
+    
+    //TOOLTIPS
+    try{
+        //JQ UI TOOLTIPS
+        $(".setLabels").tooltip();
+    }catch(err){
+        $('.advice_notice').html('Error - please update your WordPress version to the latest version. If the problem continues, contact us at support@contactus.com. ' + err ).slideToggle().delay(2000).fadeOut(2000);
+    }
+
+    //UNLINK ACCOUNT AND DELETE PLUGIN OPTIONS AND SETTINGS
+    cUsCF_myjq('.LogoutUser').click(function(){
+
+        bootbox.confirm("Do you want to unlink your account? <span class='loading'></span>", function(result) {
+            if(result){
+                cUsCF_myjq('.loading').fadeIn();
+                cUsCF_myjq.ajax({ type: "POST", url: ajax_object.ajax_url, data: {action:'cUsCF_logoutUser'},
+                    success: function(data) {
+                        cUsCF_myjq('.loading').fadeOut();
+                        location.reload();
+                    }
+                });
+            }
+        });
+
+    });
+
+
+    
+});//ON LOAD
